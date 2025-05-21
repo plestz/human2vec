5dc6a1fa99ec4983b5d79c085486846188703659
plestz <paullestzcode@gmail.com>
2025-05-21 18:15:04 -0400

    [feat] set up referral templates

--- generate_data.py
+++ generate_data.py
@@ -15,6 +15,16 @@ def generate_names(num_names: int) -> list:
     names = list({fake.name() for _ in range(num_names * 3)}) # Generate extra in case of duplicates
     return names[:num_names]
 
+def generate_referrals(num_referrals: int) -> list:
+    """
+    Generate a list of random referrals.
+    """
+    referrals = set()
+
+    
+
+    return referrals[:num_referrals]
+
 if __name__ == '__main__':
 
     num_names = 10
