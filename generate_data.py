175b94920bbf5430f48348da0ff409dbbd5e96ca
plestz <paullestzcode@gmail.com>
2025-05-21 18:02:35 -0400

    [feat] generating names functionality

---
+++ generate_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+import random
+
+from faker import Faker
+Faker.seed(42)
+random.seed(42)
+
+def generate_names(num_names: int) -> list:
+    """
+    Generate a list of random names.
+    """
+    fake = Faker()
+    names = list({fake.name() for _ in range(num_names * 3)}) # Generate extra in case of duplicates
+    return names[:num_names]
+
+if __name__ == '__main__':
+
+    num_names = 10
+    member_names = generate_names(num_names)
+    print(member_names)
