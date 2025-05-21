572f721308f71c73e5447348baaa746957b28786
plestz <paullestzcode@gmail.com>
2025-05-21 18:24:51 -0400

    [feat] generating referrals functionality

--- generate_data.py
+++ generate_data.py
@@ -12,21 +12,39 @@ def generate_names(num_names: int) -> list:
     Generate a list of random names.
     """
     fake = Faker()
-    names = list({fake.name() for _ in range(num_names * 3)}) # Generate extra in case of duplicates
-    return names[:num_names]
 
-def generate_referrals(num_referrals: int) -> list:
+    names = set()
+
+    while len(names) < num_names:
+        name = fake.name()
+        names.add(name)
+
+    return list(names)
+
+def generate_referrals(num_referrals: int, member_names: list) -> list:
     """
     Generate a list of random referrals.
     """
     referrals = set()
 
+    with open("data/referral_templates.json", "r") as f:
+        referral_templates = pd.read_json(f)['template'].tolist()
     
+    while len(referrals) < num_referrals:
+        referral_template = random.choice(referral_templates)
+        referred_member = random.choice(member_names).split(" ")[0].lower()
+        referral = referral_template.format(referred_member = referred_member)
 
-    return referrals[:num_referrals]
+        referrals.add(referral)
+
+    return list(referrals)
 
 if __name__ == '__main__':
 
-    num_names = 10
-    member_names = generate_names(num_names)
-    print(member_names)
+    NUM_NAMES = 10
+    member_names = generate_names(NUM_NAMES)
+
+    NUM_REFERRALS = 10
+    referrals = generate_referrals(NUM_REFERRALS, member_names)
+
+    print(referrals[:2])
