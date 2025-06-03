92b9782ff85ae1e2a995ef858b6ef24140120e32
plestz <paullestzcode@gmail.com>
2025-06-03 07:17:20 -1000

    [feat] generate_referrals now includes dynamic generation count capability based on existing template count and a no-reuse flag to ensure further uniqueness

--- generate_data.py
+++ generate_data.py
@@ -12,6 +12,8 @@ random.seed(42)
 NUM_NAMES = 75
 NUM_REFERRALS = 100
 
+
+# ---- GENERATE DATA ----
 def generate_first_names(num_names: int) -> list:
     """
     Generate a list of random names.
@@ -33,16 +35,17 @@ def generate_first_names(num_names: int) -> list:
 
     return list(first_names)
 
-def generate_referrals(num_referrals: int, member_first_names: list) -> list:
+def generate_referrals(member_first_names: list, num_referrals: int = None, no_template_reuse: bool = False) -> list:
     """
     Generate a list of random referrals.
 
     Args:
-        num_referrals - The number of referrals to generate
         member_names - A list of member names to use for generating referrals
+        num_referrals - The number of referrals to generate. If None, generate as many as there are templates.
+        no_template_reuse - If True, do not reuse referral templates once they have been used
         
     Returns:
-        list(referrals) - A list of random referrals based on the given member names
+        list(referrals) - A list of random template-originating referrals based on the given member names
     """
     referrals = set()
 
@@ -55,49 +58,66 @@ def generate_referrals(num_referrals: int, member_first_names: list) -> list:
     # Open the set of referral templates to generate from
     with open("data/preprocess/referral_templates.json", "r") as f:
         referral_templates = pd.read_json(f)['template'].tolist()
+
+    # If template reuse is disabled and we have a specified number of referrals, 
+    # ensure that the number of referrals does not exceed the number of templates.
+    if no_template_reuse and num_referrals is not None:
+        assert num_referrals <= len(referral_templates)
     
+    # If no number of referrals is specified, use the number of templates
+    referrals_to_generate = len(referral_templates) if num_referrals is None else num_referrals
+
     # Until we have the desired number of unique referrals, generate more
-    while len(referrals) < num_referrals:
+    while len(referrals) < referrals_to_generate:
 
         referral_template = random.choice(referral_templates)
 
         # To who is the referral being given?
         referred_member = random.choice(member_first_names)
         # From who is the referral being given?
-        non_referred_member = member_first_names.copy()
-        non_referred_member.remove(referred_member)
-        referring_member = random.choice(non_referred_member)
+        non_referred_members = member_first_names.copy()
+        non_referred_members.remove(referred_member)
+        referring_member = random.choice(non_referred_members)
 
         referral = referral_template.format(referred_member = referred_member)
 
+        # If the same exact referral has already been generated, skip it
         if referral in referrals:
             continue
 
         referrals.add(referral)
 
+        # Map each referral-receiving member to the referrals they have received
         member_to_referrals_received[referred_member].append(referral)
 
+        # Map each referring member to the members they have referred
         member_to_member_referrals_given[referring_member].append(referred_member)
 
-    with open(f"data/process/member_to_referrals_received_{NUM_NAMES}_{NUM_REFERRALS}.json", "w") as f:
+        # If we are not reusing templates, remove the template from the list of available templates for future selection
+        if no_template_reuse:
+            referral_templates.remove(referral_template)
+
+    with open(f"data/process/member_to_referrals_received_{NUM_NAMES}N_{NUM_REFERRALS}R.json", "w") as f:
         json.dump(member_to_referrals_received, f, indent = 4)
 
-    with open(f"data/process/member_to_member_referrals_given_{NUM_NAMES}_{NUM_REFERRALS}.json", "w") as f:
+    with open(f"data/process/member_to_member_referrals_given_{NUM_NAMES}N_{NUM_REFERRALS}R.json", "w") as f:
         json.dump(member_to_member_referrals_given, f, indent = 4)
 
     return list(referrals)
 
+
+# ---- MAIN ----
 if __name__ == '__main__':
 
     member_first_names = generate_first_names(NUM_NAMES)
-    referrals = generate_referrals(NUM_REFERRALS, member_first_names)
+    referrals = generate_referrals(member_first_names, NUM_REFERRALS)
 
     # Save distinct member names
-    with open(f'data/preprocess/member_first_names_{NUM_NAMES}_{NUM_REFERRALS}.txt', 'w') as f:
+    with open(f'data/preprocess/member_first_names_{NUM_NAMES}N_{NUM_REFERRALS}R.txt', 'w') as f:
         for name in member_first_names:
             f.write(f"{name}\n")
     
     # Save distinct referrals
-    with open(f'data/preprocess/referrals_{NUM_NAMES}_{NUM_REFERRALS}.txt', 'w') as f:
+    with open(f'data/preprocess/referrals_{NUM_NAMES}N_{NUM_REFERRALS}R.txt', 'w') as f:
         for referral in referrals:
             f.write(f"{referral}\n")
